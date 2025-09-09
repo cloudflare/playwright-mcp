53920529464241e223c7197effc12e289f61fe93
Rui Figueira <rfigueira@cloudflare.com>
2025-09-09 16:21:15 +0100

    chore: update @cloudflare/playwright to latest PR version

--- cloudflare/vite.config.ts
+++ cloudflare/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
       'zlib': 'node:zlib',
 
       // imported from src/tools/utils.ts
-      'playwright-core/lib/utils': path.resolve(__dirname, './node_modules/@cloudflare/playwright/lib/esm/packages/playwright-core/src/utils/isomorphic/locatorGenerators.js'),
+      'playwright-core/lib/utils': path.resolve(__dirname, './node_modules/@cloudflare/playwright/lib/packages/playwright-core/src/utils/isomorphic/locatorGenerators.js'),
 
       'playwright-core': '@cloudflare/playwright',
       'playwright': '@cloudflare/playwright/test',
